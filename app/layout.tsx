2a5a58e07e4dabb7aa2831949ada8451b5be6c38
Faridul Haque Murshed <faridmurshed9@gmail.com>
2025-12-26 13:37:02 +0600

    title updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Farid Murshed",
+  title: "Faridul Haque Murshed",
   description: "A passionate fullstack web developer",
 };
 
